46c5c2f99d4207ba06d5759a4e504ce4040b5c5a
suhailpgm <suhailpgm67@gmail.com>
2024-12-01 14:01:14 -0500

    Show total cart items count

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -8,7 +8,12 @@ function ProductList() {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
     const dispatch = useDispatch();
-     const totalQuantity = useSelector((state) => state.cart.totalQuantity);
+
+    const cartItems = useSelector((state) => state.cart?.items || []);
+    const cartCount = cartItems.reduce(
+        (total, item) => total + item.quantity,
+        0
+    );
 
    
 
@@ -287,6 +292,7 @@ const handlePlantsClick = (e) => {
                                     <circle cx="184" cy="216" r="12"></circle>
                                     <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
                                 </svg>
+                                {cartCount > 0 && <span>{cartCount}</span>}
                             </h1>
                         </a>
                     </div>
